db88e5822a7933b35ad30c57d7ccbdeb46271b56
Ryan <627311610@qq.com>
2022-04-28 23:41:02 +0800

    update: medium 1978

--- questions/1978-medium-percentage-parser/template.ts
+++ questions/1978-medium-percentage-parser/template.ts
@@ -1,1 +1,9 @@
-type PercentageParser<A extends string> = any
+type PercentageParser<A extends string, T extends [string, string, string] = ['', '', '']> = A extends `${infer L}${infer R}`
+  ? L extends '+' | '-'
+    ? PercentageParser<R, [L, '', '']>
+    : R extends '%'
+      ? [T[0], `${T[1]}${L}`, R]
+      : PercentageParser<R, [T[0], `${T[1]}${L}`, R]>
+  : T
+
+type result1978 = PercentageParser<'+100%'>
