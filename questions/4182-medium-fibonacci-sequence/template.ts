6c4e10770ef1bb7a5b7ac8d97ffd0c69b1db23f3
Ryan <627311610@qq.com>
2022-05-19 20:24:21 +0800

    update: medium 4182

--- questions/4182-medium-fibonacci-sequence/template.ts
+++ questions/4182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,5 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<T extends number, S extends never[][] = [[never], [never]]> = T extends 1 | 2
+  ? 1
+  : T extends S['length']
+  ? S[0]['length']
+  : Fibonacci<T, [[...S[0], ...S[1]], ...S]>
