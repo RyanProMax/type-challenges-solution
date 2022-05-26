bb99d316fe1c697846d58f291f2a9c01c8655822
Ryan <627311610@qq.com>
2022-05-26 10:49:48 +0800

    update: medium 4499

--- questions/4499-medium-chunk/template.ts
+++ questions/4499-medium-chunk/template.ts
@@ -1,1 +1,11 @@
-type Chunk = any
+type Chunk<A extends unknown[], L extends number, R extends unknown[] = [], I extends unknown[] = []> = A extends [infer AL, ...infer AR]
+  ? R[I['length']] extends unknown[] 
+    ? R[I['length']]['length'] extends L
+      ? Chunk<A, L, R, [...I, any]>
+      : R extends [...infer RL, infer RR]
+        ? RR extends unknown[]
+            ? Chunk<AR, L, [...RL, [...RR, AL]], I>
+            : never
+        : never
+    : Chunk<A, L, [...R, []], I>
+  : R
