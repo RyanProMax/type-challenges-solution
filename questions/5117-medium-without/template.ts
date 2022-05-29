ac654486b5d3e959fa087be3e543662ce70de73a
Ryan <627311610@qq.com>
2022-05-29 10:51:32 +0800

    update: medium 5117

--- questions/5117-medium-without/template.ts
+++ questions/5117-medium-without/template.ts
@@ -1,1 +1,9 @@
-type Without<T, U> = any
+type GenerateArray<T> = T extends unknown[] ? T : [T]
+
+type Without<T extends unknown[], U, R extends unknown[] = []> = T extends [infer TL, ...infer TR]
+  ? TL extends GenerateArray<U>[number]
+  ? Without<TR, U, R>
+  : Without<TR, U, [...R, TL]>
+  : R
+
+type result5117 = Without<[1, 2], 1>;
